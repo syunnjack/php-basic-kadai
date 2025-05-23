a8669ba20c35da19339eadb9664dc034f33fbf5f
syunnjack <syunnjack@gmail.com>
2025-05-23 23:31:29 +0900

    Add kadai_005 PHP課題（合計点と平均点）

---
+++ kadai_005/score.php
@@ -0,0 +1,23 @@
+<?php
+// 各生徒の点数を変数に代入
+$score1 = 80;
+$score2 = 60;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+// 合計点を計算
+$total = $score1 + $score2 + $score3 + $score4 + $score5 +
+         $score6 + $score7 + $score8 + $score9 + $score10;
+
+// 平均点を計算
+$average = $total / 10;
+
+// 平均点を出力
+echo "平均点は " . $average . " 点です。";
+?>
